8db343134a8bfdfa21534fa59cb2b317f4601aa1
grizzsr <justincllj@gmail.com>
2019-10-15 09:52:19 -0600

    updated script use better html method

--- myScript.js
+++ myScript.js
@@ -7,30 +7,46 @@ class todoCards {
     }
 }
 
-let todoCard = `<div class="card">
-        <div class="deleteCard" onclick="cardDelete.call(this)"><i class="far fa-times-circle"></i></div>
-        <div class="cardTitle" ><input onkeyup="addTitle.call(this)" type="text" placeholder="ToDo Title">
-        </div>
-        <div class="itemContainer">
-        <div class="addItem" onclick="addItems()"><i class="far fa-plus-square"></i>
-        </div>
-        </div>
-        </div>`;
-
 let cardItems = `<div class="cardItem"><input type="checkbox"><input type="text" class="itemTitle" placeholder="Add Item"</div>`;
-
 let cardArray = [];
+let SAVED_CARD_DATA = "savedTodoLists";
 
-//self invoking function here that grabs local storage and adds to cardArray
+
+
+
+(function cardDataRefresh() {
+    let cardData = localStorage.getItem(SAVED_CARD_DATA);
+    let parsedCard = JSON.parse(cardData);
+    for ( let i = 0; i < parsedCard.length; i++ ) {
+        let theCard = new todoCards(parsedCard[i].todoid, parsedCard[i].todoTitle, parsedCard[i].dateCreated, parsedCard[i].todoItems)
+        cardArray.push(theCard);
+        //for ( i = 0; parsedCard[i].todoItems)
+        $(".cardcontainer").append(`<div class="card" id="${parsedCard[i].todoid}">
+                    <div class="deleteCard" onclick="cardDelete.call(this)"><i class="far fa-times-circle"></i></div>
+                    <div class="cardTitle"><input value="${parsedCard[i].todoTitle}" id="cardTitleInput" onkeyup="addTitle.call(this)" type="text" placeholder="ToDo Title">
+                    </div>
+                    <div class="itemContainer">
+                        <div class="addItem" onclick="printItem()"><i class="far fa-plus-square"></i>
+                        </div>
+                    </div>
+                </div>`);
+    }
+})();
 
 function createCard() {
-    $(".cardContainer").append(todoCard);
     let newid = Math.floor(Math.random() * 99999999);
     let newCard =  new todoCards(newid, '',Date(),'');
     cardArray.push(newCard);
-    $(".card:last").attr('id', newid);
-    JSON.stringify(cardArray);
-    localStorage.setItem("cardData", cardArray);
+    $(".cardContainer").append(`<div class="card" id="${newid}">
+                    <div class="deleteCard" onclick="cardDelete.call(this)"><i class="far fa-times-circle"></i></div>
+                    <div class="cardTitle"><input id="cardTitleInput" onkeyup="addTitle.call(this)" type="text" placeholder="ToDo Title">
+                    </div>
+                    <div class="itemContainer">
+                        <div class="addItem" onclick="printItem()"><i class="far fa-plus-square"></i>
+                        </div>
+                    </div>
+                </div>`);
+    saveCards();
 
     console.log(newid);
     console.log(newCard);
@@ -45,8 +61,7 @@ function cardDelete() {
             cardArray.splice(i, 1);
             $("#" + toDelete).fadeOut( function(){
                 $("#" + toDelete).remove();
-                JSON.stringify(cardArray);
-                localStorage.setItem("cardData", cardArray);
+                saveCards()
             })
         }
         }
@@ -59,27 +74,26 @@ function addTitle() {
     for (let i = 0; i < cardArray.length; i++) {
         if(titleID == cardArray[i].todoid) {
             cardArray[i].todoTitle = titleVal;
+            saveCards();
         }
         }
     }
 
-function addItems() {
-    $(".itemContainer").prepend(cardItems);
+function printItem() {
+    $(".itemContainer").appendChild(`<div class="cardItem"><input type="checkbox"><input type="text" placeholder="Add Item"</div>`);
 
 }
+
+function saveCards() {
+    let savedData = JSON.stringify(cardArray);
+    localStorage.setItem(SAVED_CARD_DATA,savedData )
+}
 //let userData = localStorage.getItem("cardData");
-//let cardArray = JSON.parse(useData);
+//let cardArray = JSON.parse(userData);
 
 
 /*
-function saveitem() {
-    switch(e.which) {
-        case: 13
-            let myval = $("#todoinput").val();
-            console.log(myval);
-            break;
-        default:
-        //do nothing
-    }
-}
+function persistSelectedChat() {
+let chatString = JSON.stringify.(selectedChat);
+localStorage.
 */
